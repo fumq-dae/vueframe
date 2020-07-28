d6fed7ed27b9d90652bdb999ba2452d31e6c9380
fumq <fumingquan86@163.com>
2020-07-28 22:04:49 +0800

    conputed

--- src/pages/demo/demo.js
+++ src/pages/demo/demo.js
@@ -9,6 +9,7 @@ export default {
             name: "fumq",
             age: 31,
             msg: "消息",
+            html:"hello world!",
             info: {
                 title: "标题",
                 subTitle: "子标题"
@@ -16,16 +17,16 @@ export default {
             list: [{
                 isDone: true,
                 name: 'Tom',
-                addr:'北京'
+                addr: '北京'
             }, {
                 isDone: false,
                 name: 'John',
-                addr:'上海'
-                
+                addr: '上海'
+
             }, {
                 isDone: false,
                 name: 'Red',
-                addr:'武汉'
+                addr: '武汉'
             }]
 
         };
@@ -36,4 +37,11 @@ export default {
     components: {
         soltTest
     },
+    //computed原理，通过Object.defineProperties将属性代理到data上去
+    computed: {
+        ["introduce"]() {
+            return `你好，我叫${this.name}，年龄${this.age}`;
+        }
+    }
+    // methods原理，通过Object.defineProperties将属性代理到组件对象上去
 }
